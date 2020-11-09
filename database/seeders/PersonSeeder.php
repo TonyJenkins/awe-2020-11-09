940427c371d9feb1b9184767fc56f6dd660eea9e
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2020-11-09 14:40:33 +0000

    Add example model

--- database/seeders/PersonSeeder.php
+++ database/seeders/PersonSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class PersonSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\Person::factory(100)->create();
+        //
     }
 }
